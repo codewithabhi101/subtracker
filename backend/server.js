29097b23fd9d1b0f01c92d41ce207a4aed729628
ABHI123RR <abhirocks.benachigere2005@gmail.com>
2026-05-29 02:08:53 +0530

    fix CORS headers

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,17 @@ const io = new Server(server, {
 
 connectDB();
 
-app.use(cors({
-  origin: '*',
-  credentials: false
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
